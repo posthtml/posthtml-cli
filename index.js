fd8894298118dd7bbe5802ac3fb2f75822edc523
Ivan Demidov <demidov@220-volt.ru>
2016-01-19 14:52:49 +0300

    first describe argvs, convert to unix end line

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 var posthtml = require('posthtml');
+var argv = require('yargs')
+	.usage('Usage: $0 [--output|-o output.html] [input.html]')
+	.example('posthtml -o ./dist/output.html ./src/input.html', 'Default example')
+	.alias('o', 'Output file')
+	.requiresArg(['o'])
+	.version(function () {
+		return [
+			'posthtml version',
+			require('./node_modules/posthtml/package.json').version
+		].join(' ');
+	}, 'v')
+	.alias('v', 'version')
+	.help('h')
+	.alias('h', 'help')
+	.argv;
