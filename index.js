f6c5a4fa94c44203df5d9979f827d888a0130e1f
Ivan Demidov <demidov@220-volt.ru>
2016-01-19 17:02:14 +0300

    add the germ of the kernel

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var posthtml = require('posthtml');
+var fs = require('fs');
 var argv = require('yargs')
-	.usage('Usage: $0 [--output|-o output.html] [input.html]')
+	.usage('Usage: $0 [--output|-o output.html] [--input|-i input.html]')
 	.example('posthtml -o ./dist/output.html ./src/input.html', 'Default example')
-	.alias('o', 'Output file')
+	.alias('i', 'input')
+	.alias('o', 'output')
 	.requiresArg(['o'])
 	.version(function () {
 		return [
@@ -14,3 +16,11 @@ var argv = require('yargs')
 	.help('h')
 	.alias('h', 'help')
 	.argv;
+
+var html = fs.readFileSync(argv.input, "utf8");
+
+posthtml()
+    .process(html)
+    .then(function (result) {
+        fs.writeFileSync(argv.output, result.html);
+    });
