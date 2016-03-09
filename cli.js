91a3d06ff77595c3484a2ff2c8ac2cf812a79a71
Ivan Demidov <demidov@220-volt.ru>
2016-03-09 10:44:23 +0300

    update indent adn usage example

--- cli.js
+++ cli.js
@@ -6,9 +6,9 @@ delete require.cache[require.resolve('yargs')];
 var posthtml = require('posthtml');
 var fs = require('fs');
 var argv = require('yargs')
-	.usage('Usage: $0 --output|-o output.html --input|-i input.html [--config|-c config.json]')
+	.usage('Usage: $0 --output|-o output.html --input|-i input.html [--config|-c config.(js|json)]')
 	.example('posthtml -o output.html -i input.html', 'Default example')
-        .demand(['o', 'i'])
+    .demand(['o', 'i'])
 	.alias('i', 'input')
 	.alias('o', 'output')
 	.pkgConf('posthtml')
