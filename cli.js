fbee998e182f40cb73808a9eba6e2fbca9a6f6a3
Ivan Demidov <demidov@220-volt.ru>
2016-03-28 17:08:02 +0300

    Fixed #16, Fixed #9, add replace and read/write folder

--- cli.js
+++ cli.js
@@ -1,13 +1,18 @@
 #!/usr/bin/env node
 
 var posthtml = require('posthtml');
+var globby = require('globby');
+var path = require('path');
+const pathExists = require('path-exists');
 var fs = require('fs');
 var argv = require('yargs')
-	.usage('Usage: $0 --output|-o output.html --input|-i input.html [--config|-c config.(js|json)]')
+	.usage('Usage: $0 --output|-o output.html/outputFolder --input|-i input.html/inputFolder [--config|-c config.(js|json)] [--replace|-r]')
 	.example('posthtml -o output.html -i input.html', 'Default example')
-    .demand(['o', 'i'])
 	.alias('i', 'input')
 	.alias('o', 'output')
+	.alias('r', 'replace')
+	.demand(['i'])
+	.array('input')
 	.pkgConf('posthtml')
 	.config('c')
 	.alias('c', 'config')
@@ -17,14 +22,57 @@ var argv = require('yargs')
 	.alias('v', 'version')
 	.help('h')
 	.alias('h', 'help')
+	.check(function (argv) {
+		if (argv.output && argv.replace) {
+			throw new Error('Both `output file` and `replace` provided: please use either --output or --replace option.');
+		}
+		if (!argv.output && !argv.replace) {
+			throw new Error('Both `output file` and `replace` missing: please use either --output or --replace option.');
+		}
+		return true;
+	})
 	.argv;
 
-// get htmls
-var html = fs.readFileSync(argv.input, 'utf8');
+function processing(file, output) {
+	// get htmls
+	var html = fs.readFileSync(file, 'utf8');
 
-// processing
-posthtml(require('posthtml-load-plugins')(argv.config))
-	.process(html)
-	.then(function (result) {
-		fs.writeFileSync(argv.output, result.html);
+	// processing
+	posthtml(require('posthtml-load-plugins')(argv.config))
+		.process(html)
+		.then(function (result) {
+			fs.writeFileSync(output, result.html);
+		});
+}
+
+function isFile(outputPath) {
+	return Boolean(path.extname(outputPath));
+}
+
+function getOutput(file) {
+	if (argv.output === undefined) {
+		return file;
+	}
+	return argv.output + path.basename(file);
+}
+
+function createFolder(outputPath) {
+	if (isFile(outputPath) === true) {
+		outputPath = path.dirname(outputPath);
+	}
+
+	if (pathExists.sync(outputPath) === false) {
+		fs.mkdirSync(outputPath);
+	}
+}
+
+globby(argv.input).then(function (files) {
+	if (argv.output !== undefined) {
+		createFolder(argv.output);
+	}
+
+	files.forEach(function (file) {
+		var output = isFile(argv.output) ? argv.output : getOutput(file);
+		processing(file, output);
 	});
+});
