7696f595c24f5ef86315b952ecd8dec7a55aade3
Ivan Demidov <demidov@220-volt.ru>
2016-11-22 11:46:56 +0300

    fix(test-cli): fix path for test folder

--- test/test-cli.js
+++ test/test-cli.js
@@ -7,7 +7,7 @@ const readPkg = require('read-pkg')
 const copy = require('cpy')
 const tempfile = require('tempfile')
 
-const cli = path.resolve('../cli.js')
+const cli = path.resolve('cli.js')
 
 function read (pathFile) {
   return new Promise((resolve, reject) => {
@@ -25,7 +25,7 @@ test('Missing required arguments -i, -o', t => {
 })
 
 test('Missing required arguments -o', t => {
-  t.throws(execa(cli, ['-i', 'fixtures/input.html']))
+  t.throws(execa(cli, ['-i', 'test/fixtures/input.html']))
 })
 
 test('Missing required arguments -i', t => {
@@ -35,7 +35,7 @@ test('Missing required arguments -i', t => {
 
 test('One of the arguments', t => {
   const filename = tempfile('.html')
-  t.throws(execa(cli, ['-o', filename, '-r', '-i', 'fixtures/input.html']))
+  t.throws(execa(cli, ['-o', filename, '-r', '-i', 'test/fixtures/input.html']))
 })
 
 test('Check version', async t => {
@@ -45,42 +45,42 @@ test('Check version', async t => {
 test('Transform html witch config in package.json', async t => {
   t.plan(2)
   const filename = tempfile('.html')
-  await execa(cli, ['-i', 'fixtures/input.html', '-o', filename])
+  await execa(cli, ['-i', 'test/fixtures/input.html', '-o', filename])
   t.true(await pathExists(filename))
-  t.is((await read('expected/output-config-pkg.html')), (await read(filename)))
+  t.is((await read('test/expected/output-config-pkg.html')), (await read(filename)))
 })
 
 test('Transform html witch indent', async t => {
   t.plan(2)
   const filename = tempfile('.html')
-  await execa(cli, ['-i', 'fixtures/input-indent.html', '-o', filename])
+  await execa(cli, ['-i', 'test/fixtures/input-indent.html', '-o', filename])
   t.true(await pathExists(filename))
-  t.is((await read('expected/output-indent.html')), (await read(filename)))
+  t.is((await read('test/expected/output-indent.html')), (await read(filename)))
 })
 
 test('Transform html witch config in file', async t => {
   t.plan(2)
   const filename = tempfile('.html')
-  await execa(cli, ['-i', 'fixtures/input.html', '-o', filename, '-c', 'fixtures/config.json'])
+  await execa(cli, ['-i', 'test/fixtures/input.html', '-o', filename, '-c', 'test/fixtures/config.json'])
   t.true(await pathExists(filename))
-  t.is((await read('expected/output-config-file.html')), (await read(filename)))
+  t.is((await read('test/expected/output-config-file.html')), (await read(filename)))
 })
 
 test('Transform html from folder', async t => {
   t.plan(2)
   const folder = await tempfile()
-  await execa(cli, ['-i', 'fixtures/*.html', '-o', `${folder}/`])
-  t.is((await read('expected/output-config-pkg.html')), (await read(`${folder}/input.html`)))
-  t.is((await read('expected/output-indent.html')), (await read(`${folder}/input-indent.html`)))
+  await execa(cli, ['-i', 'test/fixtures/*.html', '-o', `${folder}/`])
+  t.is((await read('test/expected/output-config-pkg.html')), (await read(`${folder}/input.html`)))
+  t.is((await read('test/expected/output-indent.html')), (await read(`${folder}/input-indent.html`)))
 })
 
 test('Transform html witch options replace', async t => {
   t.plan(2)
   const folder = await tempfile()
-  await copy(['fixtures/*.html'], `${folder}/`)
+  await copy(['test/fixtures/*.html'], `${folder}/`)
   await execa(cli, ['-i', `${folder}/*.html`, '-r'])
-  t.is((await read('expected/output-config-pkg.html')), (await read(`${folder}/input.html`)))
-  t.is((await read('expected/output-indent.html')), (await read(`${folder}/input-indent.html`)))
+  t.is((await read('test/expected/output-config-pkg.html')), (await read(`${folder}/input.html`)))
+  t.is((await read('test/expected/output-indent.html')), (await read(`${folder}/input-indent.html`)))
 })
 
 test('Transform html witch config in file and stdin options use', async t => {
@@ -90,9 +90,9 @@ test('Transform html witch config in file and stdin options use', async t => {
     '-o',
     filename,
     '-i',
-    'fixtures/input-bem.html',
+    'test/fixtures/input-bem.html',
     '-c',
-    'fixtures/config.json',
+    'test/fixtures/config.json',
     '-u',
     'posthtml-bem',
     '--posthtml-bem.elemPrefix',
@@ -103,7 +103,7 @@ test('Transform html witch config in file and stdin options use', async t => {
     'posthtml-custom-elements'
   ])
   t.true(await pathExists(filename))
-  t.is((await read('expected/output-bem.html')), (await read(filename)))
+  t.is((await read('test/expected/output-bem.html')), (await read(filename)))
 })
 
 test('Transform html stdin options use witch modules', async t => {
@@ -113,12 +113,12 @@ test('Transform html stdin options use witch modules', async t => {
     '-o',
     filename,
     '-i',
-    'fixtures/input-modules.html',
+    'test/fixtures/input-modules.html',
     '-u',
     'posthtml-css-modules',
     '--posthtml-css-modules',
-    'fixtures/css-modules.json'
+    'test/fixtures/css-modules.json'
   ])
   t.true(await pathExists(filename))
-  t.is((await read('expected/output-modules.html')), (await read(filename)))
+  t.is((await read('test/expected/output-modules.html')), (await read(filename)))
 })
