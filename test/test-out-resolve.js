588ee3023501718c306c1ab1809c5f4f6c57fd6a
Ivan Demidov <scrum@list.ru>
2018-02-19 12:45:47 +0300

    test: update to Promise

--- test/test-out-resolve.js
+++ test/test-out-resolve.js
@@ -1,35 +1,40 @@
 import test from 'ava';
+import isPromise from 'is-promise';
 import outResolve from '../src/out-resolve';
 
 test('should return function', t => {
   t.true(typeof outResolve === 'function');
 });
 
-test('only input should return file.ext', t => {
-  t.is(outResolve('file.ext'), 'file.ext');
+test('should return Promise', t => {
+  t.true(isPromise(outResolve()));
 });
 
-test('only input should return tmp/file.ext', t => {
-  t.is(outResolve('tmp/file.ext'), 'tmp/file.ext');
+test('only input should return file.ext', async t => {
+  t.is(await outResolve('file.ext'), 'file.ext');
 });
 
-test('input file and output file should return output.ext', t => {
-  t.is(outResolve('file.ext', 'output.ext'), 'output.ext');
+test('only input should return tmp/file.ext', async t => {
+  t.is(await outResolve('tmp/file.ext'), 'tmp/file.ext');
 });
 
-test('input file and output folder should return tmp/file.ext', t => {
-  t.is(outResolve('file.ext', 'tmp'), 'tmp/file.ext');
+test('input file and output file should return output.ext', async t => {
+  t.is(await outResolve('file.ext', 'output.ext'), 'output.ext');
 });
 
-test('input files and output folder should return tmp/test/*.ext', t => {
-  t.is(outResolve('test/*.ext', 'tmp'), 'tmp/test/*.ext');
+test('input file and output folder should return tmp/file.ext', async t => {
+  t.is(await outResolve('file.ext', 'tmp'), 'tmp/file.ext');
 });
 
-test('input files and output file should return output.ext', t => {
-  t.is(outResolve('test/*', 'output.ext'), 'output.ext');
+test('input files and output folder should return tmp/test/*.ext', async t => {
+  t.is(await outResolve('test/*.ext', 'tmp'), 'tmp/test/*.ext');
 });
 
-test('input files and output file should return tmp/output.ext', t => {
-  t.is(outResolve('test/*', 'tmp/output.ext'), 'tmp/output.ext');
+test('input files and output file should return output.ext', async t => {
+  t.is(await outResolve('test/*', 'output.ext'), 'output.ext');
+});
+
+test('input files and output file should return tmp/output.ext', async t => {
+  t.is(await outResolve('test/*', 'tmp/output.ext'), 'tmp/output.ext');
 });
 
