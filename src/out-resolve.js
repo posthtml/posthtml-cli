5b2de2931c4d060ebde78163cb73da005d23ec21
Ivan Demidov <scrum@list.ru>
2018-02-19 12:11:28 +0300

    style: indent

--- src/out-resolve.js
+++ src/out-resolve.js
@@ -1,13 +1,13 @@
 import path from 'path';
 
 export default (input, output) => {
-    if (output && path.extname(output)) {
-        return output;
-    }
+  if (output && path.extname(output)) {
+    return output;
+  }
 
-    if (output) {
-        return path.join(output, input);
-    }
+  if (output) {
+    return path.join(output, input);
+  }
 
-    return input;
+  return input;
 };
