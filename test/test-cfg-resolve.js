d76f655d2ccbe06777bb05242390426415bd0b9c
Ivan Demidov <Scrum@list.ru>
2020-08-18 10:55:09 +0300

    test: fix input file param, issue #308,#313

--- test/test-cfg-resolve.js
+++ test/test-cfg-resolve.js
@@ -1,4 +1,5 @@
 import test from 'ava';
+import path from 'path';
 import cfgResolve from '../lib/cfg-resolve';
 
 test('should return function', t => {
@@ -13,77 +14,83 @@ test('should throw error `input files not found`', t => {
   t.is(error.message, 'input files not found');
 });
 
-test('should return config with one use key without property', t => {
+test('should return config plugins with one use key without property', t => {
+  const input = 'input.html';
   const flags = {
     use: 'posthtml-bem'
   };
-  const config = cfgResolve({flags});
+  const config = cfgResolve({input, flags});
   const expected = {'posthtml-bem': {}};
 
   t.deepEqual(config.plugins, expected);
 });
 
 test('should return config with one use key with one property', t => {
+  const input = 'input.html';
   const flags = {
     use: 'posthtml-bem',
     posthtmlBem: {
       prefix: '__'
     }
   };
-  const config = cfgResolve({flags});
+  const config = cfgResolve({input, flags});
   const expected = {'posthtml-bem': {prefix: '__'}};
 
   t.deepEqual(config.plugins, expected);
 });
 
 test('should return config with key config plugins', t => {
+  const input = 'input.html';
   const flags = {
     config: 'test/config/.config-plugins'
   };
-  const config = cfgResolve({flags});
+  const config = cfgResolve({input, flags});
   const expected = {'posthtml-bem': {}};
 
   t.deepEqual(config.plugins, expected);
 });
 
 test('should return config with key config options', t => {
+  const input = 'input.html';
   const flags = {
     config: 'test/config/.config-options'
   };
-  const config = cfgResolve({flags});
+  const config = cfgResolve({input, flags});
   const expected = {sync: true};
 
   t.deepEqual(config.options, expected);
 });
 
 test('should return config options', t => {
+  const input = 'input.html';
   const flags = {
     options: {sync: true}
   };
-  const config = cfgResolve({flags});
+  const config = cfgResolve({input, flags});
   const expected = {sync: true};
 
   t.deepEqual(config.options, expected);
 });
 
 test('should return config with key config and use key', t => {
+  const input = 'input.html';
   const flags = {
     use: 'posthtml-assets',
     config: 'test/config/.config-plugins'
   };
-  const config = cfgResolve({flags});
+  const config = cfgResolve({input, flags});
   const expected = {'posthtml-bem': {}, 'posthtml-assets': {}};
 
   t.deepEqual(config.plugins, expected);
 });
 
-test('should return config when CLI params priority', t => {
+test('should return config when CLI input param priority', t => {
   const input = 'src/template/**/*.html';
   const flags = {
     config: 'test/config/.config-input-priority'
   };
   const config = cfgResolve({input, flags});
-  const expected = 'src/template/**/*.html';
+  const expected = [path.resolve('src/template/**/*.html')];
 
   t.deepEqual(config.input, expected);
 });
