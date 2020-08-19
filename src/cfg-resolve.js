bb3973a52de6ec2a29860481864ee37bfbcffb18
Giuseppe Gurgone <giuseppeg@users.noreply.github.com>
2020-08-19 10:52:52 +0200

    Fix order for plugin set with config and cli

--- src/cfg-resolve.js
+++ src/cfg-resolve.js
@@ -19,7 +19,31 @@ export default ({input, flags = {}}) => {
   }
 
   if (use) {
-    use = [].concat(use).reduce((cfg, key) => mergeOptions(cfg, {plugins: {[key]: flags[toCamelCase(key)] || {}}}), {});
+    const configPluginOptions = config?.plugins ?? {};
+
+    // Plugins defined via CLI options take precedence over the ones from config file.
+    use = [].concat(use).reduce((cfg, name) => {
+      let cliOptions = flags[toCamelCase(name)];
+      let configOptions = configPluginOptions[name];
+      // We merge this way because options can be both strings and objects.
+      const merged = mergeOptions({ [name]: configOptions }, { [name]: cliOptions || {}});
+      // Assigning as we loop `use` makes sure that the order in cfg.plugins is correct.
+      cfg.plugins[name] = merged[name];
+      if (configOptions) {
+        delete configPluginOptions[name];
+      }
+      return cfg;
+    }, { plugins: {} });
+
+    // Add the remaining plugins if there is any.
+    if (config && config.plugins) {
+      for (let name in configPluginOptions) {
+        use.plugins[name] = configPluginOptions[name];
+      }
+      // Now all the plugins are in `use.plugins`.
+      // Delete `config.plugins` for correct merging later: mergeOptions(config, {...}, use)
+      delete config.plugins;
+    }
   }
 
   if (!config && !use) {
